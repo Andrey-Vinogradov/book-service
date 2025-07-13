90d0fb60eb645f6cfc3d1aa8d24180497e887673
Andrei_Vinahradau <Andrei_Vinahradau@epam.com>
2025-07-13 09:37:29 +0300

    Initial project structure with Book, BookService and Main

---
+++ untitled/src/main/java/com/example/bookservice/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private final int id;
+    private final String name;
+    private final String author;
+    private final String genre;
+
+    public Book(int id, String name, String author, String genre) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+    }
+
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getAuthor() { return author; }
+    public String getGenre() { return genre; }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
