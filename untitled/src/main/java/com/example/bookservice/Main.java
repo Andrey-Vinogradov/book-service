ca7fac31efc7871af0ce678d46b5f65300387423
Andrei_Vinahradau <Andrei_Vinahradau@epam.com>
2025-07-13 10:51:02 +0300

    Completely changed the principles of formation of the ID field. Now the ID is not available to the user and is completely formed programmatically.

--- untitled/src/main/java/com/example/bookservice/Main.java
+++ untitled/src/main/java/com/example/bookservice/Main.java
@@ -1,13 +1,15 @@
+package com.example.bookservice;
+
 public class Main {
     public static void main(String[] args) {
         BookService service = new BookService();
 
-        service.addBook(new Book(1, "Clean Code", "Robert Martin", "textbooks"));
-        service.addBook(new Book(2, "Effective Java", "Joshua Bloch", "textbooks"));
-        service.addBook(new Book(3, "Not Effective Java", "Joshua Bloch", "fantasy"));
-        service.addBook(new Book(0, "The best Java", "Joshua Bloch", "fantasy"));
-        service.addBook(new Book(5, "The best of the Java", "Joshua Mainer", "fantasy"));
-
+        // Добавляем книги (теперь без указания ID)
+        service.addBook("Clean Code", "Robert Martin", "textbooks");
+        service.addBook("Effective Java", "Joshua Bloch", "textbooks");
+        service.addBook("Not Effective Java", "Joshua Bloch", "fantasy");
+        service.addBook("The best Java", "Joshua Bloch", "fantasy");
+        service.addBook("The best of the Java", "Joshua Mainer", "fantasy");
 
         System.out.println(service.findById(1));
         System.out.println("*****");
@@ -19,11 +21,14 @@ public class Main {
         System.out.println("*****");
         System.out.println(service.getAllBooks());
         System.out.println("*****");
+
+        // Удаляем книгу с ID 3
         service.deleteBook(3);
         System.out.println("*****");
         System.out.println(service.getAllBooks());
-        service.addBook(new Book(0, "Next book of the Java", "Kevin Mainer", "fantasy"));
-        System.out.println(service.getAllBooks());
 
+        // Добавляем новую книгу - она должна получить ID 3 (освобожденный)
+        service.addBook("Next book of the Java", "Kevin Mainer", "fantasy");
+        System.out.println(service.getAllBooks());
     }
 }
