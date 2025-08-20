a85e4645c3e1701f92f0c807122900ebcff3fb4e
Andrei Vinahradau <veter3003g@gmail.com>
2025-08-20 17:44:18 +0300

    Moved "findByName", "findByAuthor","findByGenre","findAll","delete"to Repository add package  "exception"

--- src/main/java/com/example/bookservice/service/BookService.java
+++ src/main/java/com/example/bookservice/service/BookService.java
@@ -1,12 +1,10 @@
 package com.example.bookservice.service;
 
 import com.example.bookservice.dto.BookDTO;
+import com.example.bookservice.exception.BookNotFoundException;
 import com.example.bookservice.model.Book;
 import com.example.bookservice.repository.BookRepository;
-import com.example.bookservice.repository.CollectionBookRepositoryImpl;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -14,9 +12,11 @@ import java.util.NoSuchElementException;
 public class BookService {
 
     private final BookRepository bookRepository;
+
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
+
     private void log(String message) {
         System.out.println("[LOG] " + message);
     }
@@ -33,35 +33,21 @@ public class BookService {
             throw new IllegalArgumentException("Name, author and genre cannot be null");
         }
 
-        BookDTO dto = new BookDTO(name, author, genre); // ID будет назначен автоматически
+        BookDTO dto = new BookDTO(name, author, genre);
         bookRepository.save(dto);
         log("Added book: " + dto);
     }
 
     public void deleteBook(int id) {
-        Book book = booksById.remove(id);
-        if (book == null) {
-            throw new NoSuchElementException("Book with ID " + id + " not found");
-        }
-
-        // Удаляем книгу из всех индексов
-        //booksByName.get(book.getName()).remove(book);
-        booksByName.remove(book.getName());
-        //booksByAuthor.get(book.getAuthor()).remove(book);
-        booksByAuthor.remove(book.getAuthor());
-        //booksByGenre.get(book.getGenre()).remove(book);
-        booksByGenre.remove(book.getGenre());
-
-        // Добавляем ID в очередь свободных
-        availableIds.add(id);
-        log("Deleted book: " + book);
+        bookRepository.delete(id);
+        log("Delete book with ID: " + id);
     }
 
     public Book getById(int id) {
         Book book = bookRepository.findById(id);
         if (book == null) {
             log("Book with ID " + id + " not found");
-            throw new NoSuchElementException("Book with ID " + id + " not found"); // BookNotFoundException - нужно свою создать
+            throw new BookNotFoundException(id);
         } else {
             log("Found book by ID: " + id);
         }
@@ -69,25 +55,25 @@ public class BookService {
     }
 
     public List<Book> findByName(String name) {
-        List<Book> books = booksByName.getOrDefault(name, Collections.emptyList());
+        List<Book> books = bookRepository.findByName(name);
         log("Found " + books.size() + " books by name: " + name);
         return books;
     }
 
     public List<Book> findByAuthor(String author) {
-        List<Book> books = booksByAuthor.getOrDefault(author, Collections.emptyList());
+        List<Book> books = bookRepository.findByAuthor(author);
         log("Found " + books.size() + " books by author: " + author);
         return books;
     }
 
     public List<Book> findByGenre(String genre) {
-        List<Book> books = booksByGenre.getOrDefault(genre, Collections.emptyList());
+        List<Book> books = bookRepository.findByGenre(genre);
         log("Found " + books.size() + " books by genre: " + genre);
         return books;
     }
 
-    public List<Book> getAllBooks() {
-        List<Book> allBooks = new ArrayList<>(booksById.values());
+    public List<Book> findAllBooks() {
+        List<Book> allBooks = bookRepository.findAll();
         log("Search all books in the library. Total number of books= " + allBooks.size());
         return allBooks;
     }
